beaca3198b089ffb829d4082e818694a18b50b1b
Mateus Colombino <syde4k@gmail.com>
2023-08-17 12:00:44 -0300

    O começo de um sonho

---
+++ Quiz 2/Controllers/RegistrarController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Quiz_2.Controllers
+{
+    public class RegistrarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Registro()
+        {
+            return View();
+        }
+        public IActionResult Rankings()
+        {
+            return View();
+        }
+        public IActionResult Dificuldade()
+        {
+            return View();
+        }
+    }
+}
